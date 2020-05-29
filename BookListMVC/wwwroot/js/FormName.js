e6acd6bf22340a6c99a308db6807de29aa3fb678
kehinde ogundeyi <kehindeogundeyi@gmail.com>
2020-05-29 21:29:30 +0100

    Add project files.

---
+++ BookListMVC/wwwroot/js/FormName.js
@@ -0,0 +1,32 @@
+﻿class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            React.createElement("form", { onSubmit: this.handleSubmit },
+                React.createElement("label", null, "Name:",
+                    React.createElement("input", { type: "text", value: this.state.value, onChange: this.handleChange })),
+                React.createElement("input", { type: "submit", value: "Submit" }))
+        );
+    }
+}
+
+ReactDOM.render(
+    React.createElement(NameForm, null),
+    document.getElementById('root')
+);
---
+++ BookListMVC/wwwroot/js/MyInfo.js
@@ -0,0 +1,35 @@
+﻿class MyData extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+    }
+
+//     <div>
+////            <h1>God's kenny</h1>
+////            <p>This is a paragraph about me....</p>
+////            <ul>
+////                <li>Canada</li>
+////                <li>UK</li>
+////                <li>US</li>
+////            </ul>
+////        </div>
+   
+
+    render() {
+        return (
+            React.createElement('div', null, ``,
+                React.createElement('h1', {}, 'God_s kenny'),
+                React.createElement('p', {}, 'This is a paragraph about me....'),
+                React.createElement('li', { id: 'li1' }, 'Canada'),
+                React.createElement('li', { id: 'li1' }, 'UK'),
+                React.createElement('li', { id: 'li1' }, 'US')
+            )
+        );
+    }
+}
+
+ReactDOM.render(
+    React.createElement(MyData, null),
+    document.getElementById('root')
+);
