4dd65e281f0dc4d019ea1207abbfdaed0e74245b
kehinde ogundeyi <kehindeogundeyi@gmail.com>
2020-06-10 22:12:16 +0100

    Implemeted reusing of module

--- BookListMVC/wwwroot/js/BusinessCard.js
+++ BookListMVC/wwwroot/js/BusinessCard.js
@@ -1,43 +1,62 @@
-﻿'use strict';
-class BusinessCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { value: '' };
+﻿var React = require('react');
+var Chat = require('./Chat.js');
 
-    }
 
-    render() {
-        const mystyle = {
-            backgroundColor: "white",
-            margin: "auto",
-            width: "50%",
-            display: "flex",
-            flexDirection: "column",
-            alignitems: "center",
-            border: "1px solid #efefef",
-            boxshadow:
-                "0 1px 1px rgba(0, 0, 0, 15), 0 10px 0 -5px #eee,0 10px 1px - 4px rgba(0,0,0,0.15),0 20px 0 -10px #eee,0 20px 1px - 9px rgba(0,0,0,0.15)",
-            padding: "30px"
-        };
-        const todoitem = {
-            justifycontent: "flex-start",
-            alignitems: "center",
-            borderbottom: "1px solid #cecece",
-            fontfamily: "Roboto, sans-serif",
-            fontweight: "100",
-        }
-        return (
-            <div style={mystyle}>
-                <div style={todoitem}>
-                    <img src='/image/1.jpeg' height="150px" width="150px" />
-                    <h3>Mr Package</h3>
-                    <p>Phone: (212) 555-6735475</p>
-                    <p>Email: Mr.package@yahoo.com </p>
-                </div>
-                
-            </div>
-        );
+function BusinessCard() {
+    const mystyle = {
+        backgroundColor: "white",
+        margin: "auto",
+        width: "50%",
+        display: "flex",
+        flexDirection: "column",
+        alignitems: "center",
+        border: "1px solid #efefef",
+        boxshadow:
+            "0 1px 1px rgba(0, 0, 0, 15), 0 10px 0 -5px #eee,0 10px 1px - 4px rgba(0,0,0,0.15),0 20px 0 -10px #eee,0 20px 1px - 9px rgba(0,0,0,0.15)",
+        padding: "30px"
+    };
+    const todoitem = {
+        justifycontent: "flex-start",
+        alignitems: "center",
+        borderbottom: "1px solid #cecece",
+        fontfamily: "Roboto, sans-serif",
+        fontweight: "100",
     }
+    return (
+        <div style={mystyle}>
+            <Chat
+                name="Mr Manager"
+                imgUrl="/image/1.jpeg"
+                phone="(212) 555-6734977"
+                email="mr.manager@gmail.com"
+            />
+            <Chat
+                name="Mr Console"
+                imgUrl="/image/2.jpeg"
+                phone="(212) 555-6734977"
+                email="mr.Console@gmail.com"
+            />
+            <Chat
+                name="Mr Explorer"
+                imgUrl="/image/3.jpeg"
+                phone="(212) 555-6734977"
+                email="mr.Explorer@gmail.com"
+            />
+            <Chat
+                name="Mr Package"
+                imgUrl="/image/4.jpeg"
+                phone="(212) 555-6734977"
+                email="mr.Package@gmail.com"
+            />
+
+
+        </div>
+    );
 }
-export default BusinessCard
 
+ReactDOM.render(
+    React.createElement(BusinessCard, null),
+    document.getElementById('root')
+);
+
+//module.exports = BusinessCard;
